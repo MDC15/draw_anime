93033b5e253466561ba48262fb6f7cedd2edc002
mdc <hoangmanh.16072001@gmail.com>
2025-05-22 17:37:29 +0700

    fix: outdate

--- lib/viewmodel/counter_viewmodel.dart
+++ lib/viewmodel/counter_viewmodel.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_app/model/counter_model.dart'; // Changed to package import
 
 class CounterViewModel extends ChangeNotifier {
-  CounterModel _counter = CounterModel();
+  final CounterModel _counter = CounterModel();
 
   int get count => _counter.count;
 
